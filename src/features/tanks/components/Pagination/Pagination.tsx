3ab386321894b67d30c098d397fa0bc67147d839
Andrew Yanushkevich <andy.yanushkevich@gmail.com>
2026-03-07 17:07:16 +0300

    fix(utils): move getPageNumbers function to utils, add jsDoc to utils function

--- src/features/tanks/components/Pagination/Pagination.tsx
+++ src/features/tanks/components/Pagination/Pagination.tsx
@@ -1,6 +1,7 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Dispatch, FC, SetStateAction } from "react";
 import styles from "./Pagination.module.css";
+import { getPageNumbers } from "@/src/features/tanks/utils/utils";
 
 interface PaginationProps {
   currentPage: number;
@@ -8,26 +9,6 @@ interface PaginationProps {
   setCurrentPage: Dispatch<SetStateAction<number>>;
 }
 
-const getPageNumbers = (totalPages: number, currentPage: number) => {
-  const pages = [];
-  const maxVisiblePages = 5;
-
-  if (totalPages <= maxVisiblePages) {
-    for (let i = 1; i <= totalPages; i++) pages.push(i);
-  } else {
-    let start = Math.max(1, currentPage - 2);
-    let end = Math.min(totalPages, currentPage + 2);
-
-    if (start === 1) {
-      end = 5;
-    } else if (end === totalPages) {
-      start = totalPages - 4;
-    }
-
-    for (let i = start; i <= end; i++) pages.push(i);
-  }
-  return pages;
-};
 
 const Pagination: FC<PaginationProps> = (props) => {
   const { currentPage, totalPages, setCurrentPage } = props;
--- src/features/tanks/utils/utils.ts
+++ src/features/tanks/utils/utils.ts
@@ -1,9 +1,9 @@
 /**
  * Normalizes a string by converting it to lowercase and removing diacritics.
  * Essential for matching tank names that contains diacritics.
- * * @param {string} str - The raw string to be processed.
+ * @param {string} str - The raw string to be processed.
  * @returns {string} The normalized, lowercase string without accents.
- * * @example
+ * @example
  * normalizeString('Löwe') // returns 'lowe'
  */
 
@@ -13,3 +13,31 @@ export function normalizeString(str: string): string {
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
 }
+
+/**
+ * Get an array of vissible page numbers
+ * @param {number} totalPages - The number of all pages
+ * @param {number} currentPage - Current page number
+ * @returns {Array<number>} - The array of visible page numbers
+ */
+
+export const getPageNumbers = (totalPages: number, currentPage: number) => {
+  const pages = [];
+  const maxVisiblePages = 5;
+
+  if (totalPages <= maxVisiblePages) {
+    for (let i = 1; i <= totalPages; i++) pages.push(i);
+  } else {
+    let start = Math.max(1, currentPage - 2);
+    let end = Math.min(totalPages, currentPage + 2);
+
+    if (start === 1) {
+      end = 5;
+    } else if (end === totalPages) {
+      start = totalPages - 4;
+    }
+
+    for (let i = start; i <= end; i++) pages.push(i);
+  }
+  return pages;
+};
